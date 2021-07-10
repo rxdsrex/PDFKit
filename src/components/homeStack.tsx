cf0b37b7f43aadfc401daf28f0851443ab9d1d3b
Rajnarayan Dutta <rxdsrex@gmail.com>
2021-07-11 04:39:47 +0530

    Fixed missing code

--- src/components/homeStack.tsx
+++ src/components/homeStack.tsx
@@ -1,50 +1,21 @@
 import React from 'react';
-import {useColorScheme} from 'react-native';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
+import {createStackNavigator} from '@react-navigation/stack';
 
-import Colors from '../colors';
-import CreateScreen from '../screens/create';
-import ModifyScreen from '../screens/modify';
+import HomeTabs from './homeTabs';
+import PickImage from '../screens/pickImage';
 
-const Tab = createMaterialBottomTabNavigator();
+const RootStack = createStackNavigator();
 
-const HomeTabs = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-  const bottomBarStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
+const HomeStack = () => {
   return (
-    <Tab.Navigator
-      initialRouteName="Create"
-      backBehavior="order"
-      activeColor="#e91e63"
-      barStyle={bottomBarStyle}>
-      <Tab.Screen
-        name="Create"
-        options={{
-          tabBarLabel: 'Create',
-          tabBarIcon: ({color}) => (
-            <Ionicons name="md-create-sharp" color={color} size={24} />
-          ),
-          tabBarAccessibilityLabel: 'Create a new PDF',
-        }}
-        component={CreateScreen}
-      />
-      <Tab.Screen
-        name="Modify"
-        options={{
-          tabBarLabel: 'Modify',
-          tabBarIcon: ({color}) => (
-            <Ionicons name="md-duplicate-sharp" color={color} size={24} />
-          ),
-          tabBarAccessibilityLabel: 'Modify an existing PDF',
-        }}
-        component={ModifyScreen}
-      />
-    </Tab.Navigator>
+    <RootStack.Navigator
+      headerMode="none"
+      initialRouteName="Home"
+      screenOptions={{headerShown: false, animationTypeForReplace: 'pop'}}>
+      <RootStack.Screen name="Home" component={HomeTabs} />
+      <RootStack.Screen name="PickImage" component={PickImage} />
+    </RootStack.Navigator>
   );
 };
 
-export default HomeTabs;
+export default HomeStack;
