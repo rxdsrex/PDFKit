f1b7437ecd671e37eadf3e631a662f002d22ec1a
Rajnarayan Dutta <rxdsrex@gmail.com>
2021-07-26 10:52:57 +0530

    Code refactoring and bug fixes.

--- src/services/permissions.ts
+++ src/services/permissions.ts
@@ -1,6 +1,80 @@
 import {PermissionsAndroid} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {Alert, BackHandler, NativeModules} from 'react-native';
+const {FilesystemNativeModule} = NativeModules;
 
-export const requestCameraPermission = () => {
+export const getCameraPermission = async () => {
+  await AsyncStorage.setItem('CameraPermitted', 'false');
+  try {
+    const gotCamPerm = await requestCameraPermission();
+    await AsyncStorage.setItem('CameraPermitted', JSON.stringify(gotCamPerm));
+  } catch (err) {
+    console.warn(err.message);
+    Alert.alert(
+      'Camera Permission Error',
+      'Something went wrong while requesting Camera Permissions.\nPlease restart the app.',
+      [
+        {
+          text: 'OK',
+          onPress: () => {
+            BackHandler.exitApp();
+          },
+        },
+      ],
+      {cancelable: false},
+    );
+  }
+};
+
+export const getStoragePermission = async () => {
+  await AsyncStorage.setItem('StoragePermitted', 'false');
+  try {
+    let gotStorePerm = false;
+    do {
+      gotStorePerm = await requestStoragePermission();
+    } while (!gotStorePerm);
+    await AsyncStorage.setItem(
+      'StoragePermitted',
+      JSON.stringify(gotStorePerm),
+    );
+  } catch (err) {
+    console.warn(err.message);
+    Alert.alert(
+      'Storage Permission Error',
+      'Something went wrong while requesting Storage Permissions.\nPlease restart the app.',
+      [
+        {
+          text: 'OK',
+          onPress: () => {
+            BackHandler.exitApp();
+          },
+        },
+      ],
+      {cancelable: false},
+    );
+  }
+};
+
+export const setStorageTreeUri = async () => {
+  const storedTreeUri = await AsyncStorage.getItem('rootFolderUri');
+  if (!storedTreeUri) {
+    Alert.alert(
+      'Select App folder',
+      'Please select the folder where you want the app to create the PDF files.',
+      [
+        {
+          text: 'OK',
+          onPress: async () => {
+            await getAccessToFolder();
+          },
+        },
+      ],
+      {cancelable: false},
+    );
+  }
+};
+
+async function requestCameraPermission() {
   return new Promise<boolean>(async (resolve, reject) => {
     try {
       const camIsGranted = await PermissionsAndroid.check(
@@ -31,9 +105,9 @@ export const requestCameraPermission = () => {
       reject(err);
     }
   });
-};
+}
 
-export const requestStoragePermission = () => {
+async function requestStoragePermission() {
   return new Promise<boolean>(async (resolve, reject) => {
     try {
       const isStorageReadGranted = await PermissionsAndroid.check(
@@ -82,4 +156,38 @@ export const requestStoragePermission = () => {
       reject(err);
     }
   });
-};
+}
+
+async function getAccessToFolder() {
+  let treeUri;
+  try {
+    const docFolderUri =
+      await FilesystemNativeModule.getDocumentsDirectoryContentUri();
+    do {
+      treeUri = await FilesystemNativeModule.askPermissionForStorage(
+        docFolderUri,
+      );
+    } while (
+      treeUri === 'E_PICKER_CANCELLED' ||
+      treeUri === 'E_ROOT_FOLDER_NOT_ALLOWED' ||
+      treeUri === 'E_NO_FOLDER_DATA_FOUND' ||
+      !treeUri
+    );
+    await AsyncStorage.setItem('rootFolderUri', treeUri);
+  } catch (err) {
+    console.warn(err.message);
+    Alert.alert(
+      'Folder Picker Error',
+      'Something went wrong while selecting folder.\nPlease restart the app.',
+      [
+        {
+          text: 'OK',
+          onPress: () => {
+            BackHandler.exitApp();
+          },
+        },
+      ],
+      {cancelable: false},
+    );
+  }
+}
